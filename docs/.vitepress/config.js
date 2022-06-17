7b6f299d19ea7190fbade119b9fc68fd7a976a94
鸭脖yaboo <863176846@qq.com>
2022-06-17 16:02:47 +0800

    feat: all note about algorithm

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vitepress'
+import AutoNavPlugin from '../../../vitepress-auto-nav-sidebar/lib/index'
+
+const { nav, sidebar } = AutoNavPlugin({
+  ignoreFolders: [
+    'node_modules',
+    'assets',
+    'public',
+    '.vitepress',
+    'code',
+    '.obsidian',
+    'utils',
+    'resource'
+  ],
+  ignoreFiles:['index'],
+  singleLayerNav:true
+})
+
+
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'Code More Create',
+  // description: 'Vite & Vue powered static site generator.',
+  themeConfig: {
+    logo: 'logo.png',
+    siteTitle: 'Code More Create',
+    nav, sidebar,
+    footer: {
+      message: 'MIT Licensed | Copyright © 2021 - 2022',
+      copyright: '粤ICP备2021165391号'
+    },
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/Merlin218'
+      }
+    ],
+    editLink: {
+      pattern: 'https://github.com/Merlin218/learning-algorithm/edit/master/docs/:path',
+      text: '更正错误'
+    },
+    lastUpdatedText: '更新时间'
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+---
+layout: home
+hero:
+  name: learn algorithm together!
+  text: Code More Create
+  actions:
+    - theme: alt
+      text: 掘金
+      link: https://juejin.cn/user/1847596772237719
+    - theme: alt
+      text: Leetcode
+      link: https://leetcode-cn.com/u/merlin218
+    - theme: alt
+      text: BiliBili
+      link: https://space.bilibili.com/8227630
+features:
+  - title: 个人博客
+    details: 包括前端基础、进阶、读书...
+  - title: 技术栈
+    details: Vue3, TypeScript, Vite ...
+--- 
